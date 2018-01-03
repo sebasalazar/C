ec983db2368e9c3a9aabe564134e64aa5fc99d50
Sebastián Salazar Molina <ssalazar@experti.cl>
2018-01-03 16:21:50 -0300

    Caso de prueba

--- examen-cp2017/probar.sh
+++ examen-cp2017/probar.sh
@@ -10,6 +10,6 @@ make || exit 1
 ./dist/integral "4x**2 + 1" 1 10
 ./dist/integral "x**3 + 25x**1"  1 10
 ./dist/integral "x**5 + 3" 1 10
-./dist/integral "x**2 + 4x**1 + 4" 1 10
+./dist/integral "x**2 + 4x**1" 1 10
 ./dist/integral "13x**13 + 7x**7 + 5x**5 + 3x**3 +1" 1 10
 
